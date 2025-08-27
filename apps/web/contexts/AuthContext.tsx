c790de5b1fba00df43768517ce08006d1762f008
EL Bonuan <112713833+EL-MALAMAYA@users.noreply.github.com>
2025-08-27 22:58:04 +0800

    Update AuthContext.tsx

--- apps/web/contexts/AuthContext.tsx
+++ apps/web/contexts/AuthContext.tsx
@@ -402,7 +402,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-  }
 }
 
 export function useAuth() {
